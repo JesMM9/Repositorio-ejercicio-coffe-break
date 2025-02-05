8fd42194f9b75d5aba71aadd48b3c683cf4ad597
JesMM9 <jes9mimi9@gmail.com>
2025-02-05 19:18:18 +0100

    Comenzado el ejercicio coffe break de java

---
+++ CoffeBreak.java
@@ -0,0 +1,7 @@
+package CoffeBreak;
+
+public class CoffeBreak {
+	
+	
+
+}
---
+++ MaquinaCafe.java
@@ -0,0 +1,18 @@
+package CoffeBreak;
+
+import java.time.LocalDateTime;
+
+public class MaquinaCafe {
+	
+	private String id;
+	private static int counter;
+	private double cafe;
+	private double leche;
+	private double chocolate;
+	private int cafesServidos;
+	private int capuccinosServidos;
+	private int chocolatesServidos;
+	private double importe;
+	private LocalDateTime ultimoEncendido;
+
+}
